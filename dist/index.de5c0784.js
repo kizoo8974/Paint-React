d3e29e472316ad243e6d9cd0e59d1ad403a55f3a
Joon Kim <kizoo8974@gamil.com>
2023-06-23 12:46:18 +0900

    drawing function

--- dist/index.de5c0784.js
+++ dist/index.de5c0784.js
@@ -1,3 +1,43 @@
-console.log("paint");
+class DrawingBoard {
+    MODE = "NONE";
+    IsMouseDown = false;
+    constructor(){
+        this.assignElement();
+        this.initContext();
+        this.addEvent();
+    }
+    assignElement() {
+        this.containerEl = document.getElementById("container");
+        this.canvasEl = this.containerEl.querySelector("#canvas");
+        this.toolbarEl = this.containerEl.querySelector("#toolbar");
+        this.brushEl = this.toolbarEl.querySelector("#brush");
+    }
+    initContext() {
+        this.context = this.canvasEl.getContext("2d");
+    }
+    addEvent() {
+        this.brushEl.addEventListener("click", this.onClickBrush.bind(this));
+        this.canvasEl.addEventListener("mousedown", this.onMouseDown.bind(this));
+    }
+    onMouseDown(event) {
+        if (this.MODE === "NONE") return;
+        this.IsMouseDown = true;
+        const currentPosition = this.getMousePosition(event);
+    }
+    getMousePosition(event) {
+        const boundaries = this.canvasEl.getBoundingClientRect();
+        return {
+            x: event.clientX - boundaries.left,
+            y: event.clientY - boundaries.top
+        };
+    }
+    onClickBrush(event) {
+        const IsActive = event.currentTarget.classList.contains("active");
+        this.MODE = IsActive ? "NONE" : "BRUSH";
+        this.canvasEl.style.cursor = IsActive ? "default" : "crosshair";
+        this.brushEl.classList.toggle("active");
+    }
+}
+new DrawingBoard();
 
 //# sourceMappingURL=index.de5c0784.js.map
