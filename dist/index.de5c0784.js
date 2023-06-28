1441e079986cee63e94030721171cbd3286420cf
Joon Kim <kizoo8974@gamil.com>
2023-06-28 12:17:24 +0900

    reset function

--- dist/index.de5c0784.js
+++ dist/index.de5c0784.js
@@ -25,6 +25,7 @@ class DrawingBoard {
         this.navigatorImageContainerEl = this.containerEl.querySelector("#imgNav");
         this.navigatorImageEl = this.navigatorImageContainerEl.querySelector("#canvasImg");
         this.undoEl = this.toolbarEl.querySelector("#undo");
+        this.clearEl = this.toolbarEl.querySelector("#clear");
     }
     initContext() {
         this.context = this.canvasEl.getContext("2d");
@@ -44,6 +45,13 @@ class DrawingBoard {
         this.eraserEl.addEventListener("click", this.onClickEraser.bind(this));
         this.navigatorEl.addEventListener("click", this.onClickNavigator.bind(this));
         this.undoEl.addEventListener("click", this.onClickUndo.bind(this));
+        this.clearEl.addEventListener("click", this.onClickClear.bind(this));
+    }
+    onClickClear() {
+        this.context.clearRect(0, 0, this.canvasEl.width, this.canvasEl.height);
+        this.undoArray = [];
+        this.updateNavigator();
+        this.initCanvasBackgroundColor();
     }
     onClickUndo() {
         if (this.undoArray.length === 0) {
