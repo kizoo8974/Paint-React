ae5e3f395cd1e6c5bc9cebac34a5881206306a31
Joon Kim <kizoo8974@gamil.com>
2023-06-28 12:07:21 +0900

    Save & undo functions

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,9 @@ class DrawingBoard {
     IsMouseDown = false; // true false
     eraserColor = "#FFFFFF"
     backgroundColor = "#FFFFFF"
+    IsNavigatorVisible = false;
+    undoArray = [];
+
     constructor() {
         this.assignElement();
         this.initContext();
@@ -23,7 +26,8 @@ class DrawingBoard {
             this.eraserEl = this.toolbarEl.querySelector("#eraser");
             this.navigatorEl =this.toolbarEl.querySelector("#navigator")
             this.navigatorImageContainerEl = this.containerEl.querySelector("#imgNav");
-            this.navigatorImageEl = this.navigatorImageContainerEl.querySelector("#canvasImg")
+            this.navigatorImageEl = this.navigatorImageContainerEl.querySelector("#canvasImg");
+            this.undoEl = this.toolbarEl.querySelector("#undo")
         }
         initContext() {
             this.context = this.canvasEl.getContext("2d")
@@ -42,15 +46,51 @@ class DrawingBoard {
             this.colorPickerEl.addEventListener("input", this.onChangeColor.bind(this));
             this.eraserEl.addEventListener("click", this.onClickEraser.bind(this));
             this.navigatorEl.addEventListener("click", this.onClickNavigator.bind(this));
+            this.undoEl.addEventListener("click", this.onClickUndo.bind(this));
         };
 
+        onClickUndo(){
+            if(this.undoArray.length === 0) {
+                alert("Save unavailable")
+                return;
+            }
+            let previousDataUrl = this.undoArray.pop()
+            let previousImage = new Image();
+            previousImage.onload = () => {
+                this.context.clearRect(0,0, this.canvasEl.width, this.canvasEl.height);
+                this.context.drawImage(previousImage, 
+                    0, 
+                    0, 
+                    this.canvasEl.width, 
+                    this.canvasEl.height, 
+                    0, 
+                    0, 
+                    this.canvasEl.width, 
+                    this.canvasEl.height
+                    );
+            };
+            previousImage.src = previousDataUrl;
+        }
+
+        saveState() {
+            if(this.undoArray.length > 4){
+                this.undoArray.shift();
+                this.undoArray.push(this.canvasEl.toDataURL());
+            } else {
+                this.undoArray.push(this.canvasEl.toDataURL());
+            }
+            
+        }
+
         onClickNavigator(event) {
+            this.IsNavigatorVisible = !event.currentTarget.classList.contains("active");
             event.currentTarget.classList.toggle("active");
             this.navigatorImageContainerEl.classList.toggle("hide");
             this.updateNavigator();
         }
 
         updateNavigator() {
+            if(!this.IsNavigatorVisible) return;
             this.navigatorImageEl.src = this.canvasEl.toDataURL();
         }
 
@@ -93,6 +133,7 @@ class DrawingBoard {
                 this.context.strokeStyle = this.eraserColor;
                 this.context.lineWidth = 50;
             }
+            this.saveState();
             // this.context.lineTo(400, 400);
             // this.context.stroke();
         }
